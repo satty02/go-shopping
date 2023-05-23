f49cb4db4627e18bcaaefe7934c982aac9729f07
satty02 <satish.pal.2996@gmail.com>
2023-05-23 20:43:50 +0530

    header  footers first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import TopNavBar from '../Components/Headers/TopNavBar'
+import Footers from '../Components/Footers/Footers'
+
+function Home() {
+  return (
+    <Fragment>
+        <TopNavBar/>
+        <Footers/>
+    </Fragment>
+    )
+}
+
+export default Home
